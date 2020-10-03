81149c84fcf04176d8bba516af53420bdaa7d81c
Andrew <andrewdfabrizio@gmail.com>
2020-10-03 15:40:21 -0400

    Updated models. Issue with ManyToMany relationships.

--- auctions/models.py
+++ auctions/models.py
@@ -6,16 +6,28 @@ class User(AbstractUser):
     username = models.CharField(max_length=64, unique=True)
     email = models.EmailField()
     password = models.CharField(max_length=256)
-    watchlist = models.ManyToManyField("Listing", blank=True, related_name="watchlist")
+    watchlist = models.ManyToManyField('Listing', blank=True, related_name='watchlist')
+    bids = models.ManyToManyField('Listing', through='Bids', through_fields=('listing', 'user'))
 
-
-# TO-DO: mess with this for next specs.
 class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField(max_length=500)
-#TO-DO: Make bids a separate model.
-    bid = models.DecimalField(max_digits=8, decimal_places=2)
-    high_bidder = models.ForeignKey('User', related_name='bidder', on_delete=models.CASCADE, blank=True, null=True)
+    starting_bid = models.DecimalField(max_digits=8, decimal_places=2)
+    bidders = models.ManyToManyField('User', through='Bids', through_fields=('listing', 'user'))
     image_url = models.URLField()
     category = models.CharField(max_length=64)
-    owner = models.ForeignKey('User', related_name='listing_owner', on_delete=models.CASCADE, blank=True, null=True)
+    owner = models.ForeignKey('User', related_name='listing_owner', on_delete=models.CASCADE)
+    Active = models.BooleanField()
+    comments = models.ForeignKey('Comments', related_name='comments', on_delete=models.CASCADE)
+
+class Bids(models.Model):
+    user = models.ForeignKey('User', related_name='bidder', on_delete=models.CASCADE)
+    listing = models.ForeignKey('Listing', related_name='listing_bid', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now=False)
+
+class Comments(models.Model):
+    author = models.ForeignKey('User', related_name='author', on_delete=models.CASCADE)
+    listing = models.ForeignKey('Listing', related_name='listing_comment', on_delete=models.CASCADE)
+    body = models.TextField(max_length=500)
+    timestamp = models.DateTimeField(auto_now=False)
