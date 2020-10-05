f96db30025009c3220883d482ce62891f32744cf
Andrew <andrewdfabrizio@gmail.com>
2020-10-04 20:35:10 -0400

    MIGRATED BABY! Fixed my many-to-many relationships in models.py.

--- auctions/models.py
+++ auctions/models.py
@@ -7,18 +7,18 @@ class User(AbstractUser):
     email = models.EmailField()
     password = models.CharField(max_length=256)
     watchlist = models.ManyToManyField('Listing', blank=True, related_name='watchlist')
-    bids = models.ManyToManyField('Listing', through='Bids', through_fields=('listing', 'user'))
+    bids = models.ManyToManyField('Listing', through='Bids', through_fields=('user', 'listing'), related_name='bids')
 
 class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField(max_length=500)
     starting_bid = models.DecimalField(max_digits=8, decimal_places=2)
-    bidders = models.ManyToManyField('User', through='Bids', through_fields=('listing', 'user'))
+    bidders = models.ManyToManyField('User', through='Bids', through_fields=('listing', 'user'), related_name='bidders')
     image_url = models.URLField()
     category = models.CharField(max_length=64)
     owner = models.ForeignKey('User', related_name='listing_owner', on_delete=models.CASCADE)
     Active = models.BooleanField()
-    comments = models.ForeignKey('Comments', related_name='comments', on_delete=models.CASCADE)
+    comments = models.ForeignKey('Comments', related_name='comments', on_delete=models.CASCADE, blank=True, null=True)
 
 class Bids(models.Model):
     user = models.ForeignKey('User', related_name='bidder', on_delete=models.CASCADE)
